7b7a7011d5d0eeca676d5b5a9a82083ecd1d4c41
hyesoeng <tjdehfldk@gmail.com>
2021-05-02 10:11:38 +0900

    remove no used line

--- titanic/dl/model/dense_net.py
+++ titanic/dl/model/dense_net.py
@@ -1,19 +1,8 @@
-from pprint import pprint
-import numpy as np
 import tensorflow as tf
-
-from tensorflow.keras import layers
-from tensorflow.keras.layers import Input, Dense, Activation, BatchNormalization, Flatten, Dropout
+from tensorflow.keras.layers import Input, Dense, BatchNormalization, Dropout
 from tensorflow.keras.models import Model
-from tensorflow.keras.metrics import Accuracy
-from tensorflow.keras.losses import BinaryCrossentropy
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
-
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split, GridSearchCV
-
 from numpy.random import seed
+
 seed(0)
 tf.random.set_seed(0)
 
